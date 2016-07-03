cee61450a88f544f58cd2e7ee80f1ec073b230f7
davidecheta599 <davidecheta599@yahoo.com>
2016-07-03 13:41:12 +0100

    this is

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Shop on line</title>
+
+<link rel="stylesheet" type="text/css" href="styles/style.css" media="all"/>
+
+</head>
+
+<body>
+<!-- -- -- -- ---main container start here- - -- --------------------------------- -->
+
+<div class="main_wrapper">
+
+
+<!-- -- -- -- ---header start here- - -- --------------------------------- -->
+<div class="header_wrapper">  
+
+
+    <div id="logo">
+			   <img  src="images/logo.jpg"/>
+			   <img  src="images/pic1.jpg"/>
+			   <img  src="images/pic2.jpg"/>
+			   <img  src="images/apple-logo.png"/>
+			   <img  src="images/pic3.jpg"/>
+			   <img  src="images/pic4.jpg"/>
+			   <img src="images/pic5.jpg"/>
+			  <img src="images/pic6.jpg"/>
+			 
+			  
+		</div> 	 													   
+</div>
+<!-- -- -- -- ---header ends here- - -- --------------------------------- -->
+  
+
+<!-- -- -- -- ---navigation menu  start here- - -- --------------------------------- -->
+  
+  
+<div class="menubar">
+  <ul id="menu">
+     <li> <a href="#">Home </a> </li>
+	 <li> <a href="#">all products </a> </li>
+	 <li> <a href="#">my account </a> </li>
+	 <li> <a href="#">sign up </a> </li>
+	 <li> <a href="#">shopping cart</a> </li>
+	 <li> <a href="#">contact us</a> </li>
+  </ul>
+<div id="form">
+              <form action="result.php" method="get" enctype="multipart/form-data">
+                 <input type="text" name="user-query" placeholder="search a product" />
+                 <input type="submit" name="search" value="serach" />
+              </form>
+</div>
+
+</div>
+
+<div class="content_wrapper">
+
+   <div id="sidebar">this is sidebar</div>
+
+   <div id="content_area">this is content area</div>
+
+</div>
+<!-- -- -- -- ---navigation menu  ends here- - -- --------------------------------- -->
+
+
+<div id="footer">this is footer</div>
+
+
+</div>
+<!-- -- -- -- ---main container ends here- - -- --------------------------------- -->
+</body>
+</html>
+
+
